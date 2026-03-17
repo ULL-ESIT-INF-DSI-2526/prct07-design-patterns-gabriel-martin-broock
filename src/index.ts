b2270ff1b03ce69fe970740e408d83c100e02577
Gabriel Martín Broock <114770294+gabmartinbr@users.noreply.github.com>
2026-03-17 12:33:57 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param a - d
+ * @param b - ds
+ * @returns s
+ */
+export function suma(a:number,b:number):number{
+const resultado = a+b;
+return resultado
+}
+
+console.log(suma(1, 2));
